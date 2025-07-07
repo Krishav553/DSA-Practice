40bb94502465047f31757132f2d2fa35557b67bd
rishav kumar <35185417+Krishav553@users.noreply.github.com>
2025-07-07 11:29:54 +0530

    Add files via upload

---
+++ src/main/java/java/DSA/DsaPracticeApplication.java
@@ -0,0 +1,13 @@
+package java.DSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaPracticeApplication.class, args);
+	}
+
+}
